db4b4ffb87eadb5f2e959b18ce319cf43a017ca2
oodi-g7 <oodig7@gmail.com>
2023-06-06 22:53:24 +0900

    section2-8 OrderTest

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L; // long을 써도 되지만 그건 null값을 담지 못해서, 혹시나 테스트 하던 중 null값이 들어갈 수도 있으므로 Long을 사용
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
