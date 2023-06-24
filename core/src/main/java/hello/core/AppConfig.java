4b26da8068b2e2c0af9c4c5fab0830c15d51da64
oodi-g7 <oodig7@gmail.com>
2023-06-24 23:50:43 +0900

    section5-5 @Configuration&Singleton

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,16 +15,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 애플리케이션의 설정,구성정보를 담당하는 곳에 어노테이션 표시
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //@Bean memberRepository -> new MemoryMemberRepository()
+
+    //예상호출
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //실제호출
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
     @Bean // 구성요소들에다 @Bean을 붙여서 전부 스프링 컨테이너에 등록시키기
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository(){
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
