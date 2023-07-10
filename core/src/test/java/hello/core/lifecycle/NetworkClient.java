15a389d845bac2fbae8fcd1e8cad6ce9d8946558
oodi-g7 <oodig7@gmail.com>
2023-07-11 00:51:10 +0900

    initMethod, destroyMethod

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,8 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements 
-        InitializingBean, DisposableBean { // 지금은 잘 쓰지 않는 방법
+public class NetworkClient {
 
     private String url;
 
@@ -30,16 +29,29 @@ public class NetworkClient implements
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { // 프로퍼티들이 세팅이 끝나면(의존관계 주입이 끝나면) 호출해주겠다.
-        System.out.println("NetworkClient.afterPropertiesSet");
+//    - InitializingBean, DisposableBean 이용
+//    @Override
+//    public void afterPropertiesSet() throws Exception { // 프로퍼티들이 세팅이 끝나면(의존관계 주입이 끝나면) 호출해주겠다.
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        disconnect();
+//    }
+
+//    - 빈 등록 초기화, 소멸 메서드 이용
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
