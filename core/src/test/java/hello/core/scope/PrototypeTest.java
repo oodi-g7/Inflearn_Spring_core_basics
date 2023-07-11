f4f0d8f30bdc9ef18f910e21ad73e8aaa7e7acc0
oodi-g7 <oodig7@gmail.com>
2023-07-11 23:09:04 +0900

    section9-2 : prototype scope

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,48 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+//        ac.close(); // 프로토타입 스코프는 스프링 컨테이너에서 관리안하므로 close메소드 동작안함
+    // 종료해줄거면 아래와같이 직접 빈을 호출하여 종료메서드 실행시키기
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
