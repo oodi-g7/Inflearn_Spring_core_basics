59e56b2da5e17439e2448f9e392e0666c6a41bf2
oodi-g7 <oodig7@gmail.com>
2023-07-13 21:53:39 +0900

    section9-4 : ObjectProvider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,8 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -35,19 +37,30 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
         
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        // (2) 필드주입방식
+        // ObjectProvider<"빈이름">의 getObject()를 호출하면 내부에서는 스프링 컨테이너를 통해 해당 빈을 찾아서 반환한다.
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider; // objectfactory도 가능.
+//                                                                       objectfactory의 자식이 objectprovider이고, objectfactory는 getobject 메소드 하나뿐이라면,
+//                                                                       objectprovider는 그 밖의 여러기능을 추가적으로 제공
+
+        
+        // (1) 생성자주입방식
+//        private final PrototypeBean prototypeBean; // 생성시점에 주입
+//        
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); //getObject()를 하고나서야 스프링 컨테이너에서 프로토타입빈을 찾아 반환해줌
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
